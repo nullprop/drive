3a6efa4385e8b6c3e78c950941489529ccd8bc68
nullprop <git@nullprop.sh>
2024-09-29 05:41:39 +0300

    Fix sun lighting surfaces from below the horizon

--- src/Shaders/include/Lighting.glsl
+++ src/Shaders/include/Lighting.glsl
@@ -3,11 +3,14 @@ vec3 BlinnPhong(vec3 fragPos, vec3 fragNormal, vec3 eyePos, float specularity)
     vec3 viewDir = normalize(eyePos - fragPos);
     vec3 halfDir = normalize(ubo.sunDir + viewDir);
 
+    float sunMult = smoothstep(-0.6, 0.1, ubo.sunDir.z);
+    vec3 sunColor = ubo.sunColor * sunMult;
+
     float diff = max(dot(fragNormal, ubo.sunDir), 0.0);
-    vec3 diffuse = diff * ubo.sunColor;
+    vec3 diffuse = diff * sunColor;
 
     float spec = pow(max(dot(fragNormal, halfDir), 0.0), 16.0);
-    vec3 specular = specularity * spec * ubo.sunColor;
+    vec3 specular = specularity * spec * sunColor;
 
     vec3 ambient = vec3(0.01);
     float horizonScatter = 1.0 - abs(ubo.sunDir.z);
